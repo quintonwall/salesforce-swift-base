e57072fb8b97453b6d2d8dbf7dbf07ef21d06255
Quinton Wall <qwall@salesforce.com>
2016-06-23 15:36:10 -0700

    removed for update to 4.0

--- README.md
+++
@@ -1,2 +0,0 @@
-# salesforce-swift-base
-Starter project configured to use Salesforce Mobile SDK 3.0, Swift, and Storyboards
--- SalesforceSwiftBase/Classes/AppDelegate.swift
+++
@@ -1,9 +0,0 @@
-//
-//  AppDelegate.swift
-//  Salesforce-Swift-Base
-//
-//  Created by Quinton Wall on 6/23/16.
-//  Copyright © 2016 Quinton Wall. All rights reserved.
-//
-
-import Foundation
